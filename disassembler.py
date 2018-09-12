39703af75fdd0e83dff789e1ee8bdc46e1adf5ec
Bo Heyse <Heyseb1@Bos-MacBook-Pro.local>
2018-09-12 09:16:22 -0500

    Added mem variable to increment memory location by 4

--- disassembler.py
+++ disassembler.py
@@ -1,15 +1,16 @@
 class disassembler(object):
-    register_codes = {'00000':'R0 ', '00001':'R1 ', '00010':'R2 ', '00011':'R3 ', '00100':'R4 ',
-                      '00101':'R5 ', '00110':'R6 ', '00111':'R7 ', '01000':'R8 ', '01001':'R9 ',
-                      '01010':'R10 ', '01011':'R11 ', '01100':'R12 ', '01101':'R13 ', '01110':'R14 ',
-                      '01111':'R15 ', '10000':'R16 ', '10001':'R17 ', '10010':'R18 ', '10011':'R19 ',
-                      '10100':'R20 ', '10101':'R21 ', '10110':'R22 ', '10111':'R23 ', '11000':'R24 ',
-                      '11001':'R25 ', '11010':'R26 ', '11011':'R27 ', '11100':'R28 ', '11101':'R29 ',
-                      '11110':'R30 ', '1111':'R31 '}
+    register_codes = {'00000':'R0', '00001':'R1', '00010':'R2', '00011':'R3', '00100':'R4',
+                      '00101':'R5', '00110':'R6', '00111':'R7', '01000':'R8', '01001':'R9 ',
+                      '01010':'R10', '01011':'R11', '01100':'R12', '01101':'R13', '01110':'R14',
+                      '01111':'R15', '10000':'R16', '10001':'R17', '10010':'R18', '10011':'R19',
+                      '10100':'R20', '10101':'R21', '10110':'R22', '10111':'R23', '11000':'R24',
+                      '11001':'R25', '11010':'R26', '11011':'R27', '11100':'R28', '11101':'R29',
+                      '11110':'R30', '1111':'R31'}
     binary_array = []
     code_array = []
     register_array = []     #holds the register or immediate value to be printed
     instruction = ""
+    mem = '96'
 
 
     def __init__(self):
@@ -24,7 +25,7 @@ class disassembler(object):
 
     def run(self):
         count = 0
-        while(count < 2):#len(self.binary_array)):
+        while(count < len(self.binary_array)):
             opcode = str(self.binary_array[count])
             decimal_opcode = int(opcode[0:11], 2)   #holds the decimal opcode value
             op_1_format = opcode[0:8] #holds the format for first 1-8 opcode digits
@@ -45,8 +46,8 @@ class disassembler(object):
 
                 self.instruction = (op_1_format + " " + op_2_format + " " + reg_1_format + " "
                                    + imm_format + " " + reg_2_format + " " + reg_3_format + "\t"
-                                   + "96\t" + "AND\t" + self.register_codes[rd]
-                                   + self.register_codes[rn] + self.register_codes[rm])
+                                   + self.mem + "\t" + "AND\t" + self.register_codes[rd] + ', '
+                                   + self.register_codes[rn] + ', ' + self.register_codes[rm])
 
                 print(self.instruction)
 
@@ -59,8 +60,8 @@ class disassembler(object):
 
                 self.instruction = (op_1_format + " " + op_2_format + " " + reg_1_format + " "
                                    + imm_format + " " + reg_2_format + " " + reg_3_format + "\t"
-                                   + "96\t" + "ADD\t" + self.register_codes[rd]
-                                   + self.register_codes[rn] + self.register_codes[rm])
+                                   + self.mem + "\t" + "ADD\t" + self.register_codes[rd] + ', '
+                                   + self.register_codes[rn] + ', ' + self.register_codes[rm])
 
                 print(self.instruction)
 
@@ -73,7 +74,13 @@ class disassembler(object):
                 shamt = opcode[16:22]
                 rn = opcode[22:27]
                 rd = opcode[27:32]
-                print('ORR')
+
+                self.instruction = (op_1_format + " " + op_2_format + " " + reg_1_format + " "
+                                   + imm_format + " " + reg_2_format + " " + reg_3_format + "\t"
+                                   + self.mem + "\t" + "ORR\t" + self.register_codes[rd] + ', '
+                                   + self.register_codes[rn] + ', ' + self.register_codes[rm])
+
+                print(self.instruction)
 
             elif(decimal_opcode >= 1440 and decimal_opcode <= 1447):
                 print('CBZ')
@@ -87,7 +94,12 @@ class disassembler(object):
                 rn = opcode[22:27]
                 rd = opcode[27:32]
 
-                print('SUB')
+                self.instruction = (op_1_format + " " + op_2_format + " " + reg_1_format + " "
+                                   + imm_format + " " + reg_2_format + " " + reg_3_format + "\t"
+                                   + self.mem + "\t" + "SUB\t" + self.register_codes[rd] + ', '
+                                   + self.register_codes[rn] + ', ' + self.register_codes[rm])
+
+                print(self.instruction)
 
             elif(decimal_opcode == 1672 or decimal_opcode == 1673):
                 print('SUBI')
@@ -118,11 +130,7 @@ class disassembler(object):
                 print(opcode)
 
             count += 1
-
-
-
-
-
+            self.mem = str(int(self.mem) + 4)
 
 
 
